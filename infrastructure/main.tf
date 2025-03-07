c45deacae7f97bb894ae38abb2536303d3a4e625
John Vinh <john.michael.vinh2@gmail.com>
2025-03-07 14:48:47 -0500

    Fix Terraform formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,9 +19,9 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name = "johnvinhcst8918storage"
-  resource_group_name = azurerm_resource_group.resource_group.name
-  location = azurerm_resource_group.resource_group.location
+  name                     = "johnvinhcst8918storage"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
   account_replication_type = "LRS"
-  account_tier = "Standard"
+  account_tier             = "Standard"
 }
