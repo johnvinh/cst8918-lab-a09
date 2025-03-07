98047aefa8f418fbddf97d5735841538185225a5
John Vinh <john.michael.vinh2@gmail.com>
2025-03-07 13:54:15 -0500

    Create basic Terraform script

---
+++ infrastructure/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "cst8918-lab-a09"
+  location = "Canada Central"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name = "johnvinhcst8918storage"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location = azurerm_resource_group.resource_group.location
+  account_replication_type = "LRS"
+  account_tier = "Standard"
+}
